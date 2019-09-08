aa7759df4fcb7862c07a240dc1f52bd7b41e98fb
Shuhao Zhang <tonyzhang19900609@gmail.com>
2019-09-08 11:56:06 +0800

    pause..
    It’s too time-consuming to continue.

---
+++ cmake-build-release/storage/CMakeFiles/DbIterator.cpp.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DbIterator.cpp.dir/Tuple.cpp.o"
+  "CMakeFiles/DbIterator.cpp.dir/TupleDesc.cpp.o"
+  "CMakeFiles/DbIterator.cpp.dir/TupleIterator.cpp.o"
+  "CMakeFiles/DbIterator.cpp.dir/type/Type.cpp.o"
+  "CMakeFiles/DbIterator.cpp.dir/Field.cpp.o"
+  "CMakeFiles/DbIterator.cpp.dir/IntField.cpp.o"
+  "CMakeFiles/DbIterator.cpp.dir/type/INT_TYPE.cpp.o"
+  "DbIterator.cpp.pdb"
+  "DbIterator.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DbIterator.cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
