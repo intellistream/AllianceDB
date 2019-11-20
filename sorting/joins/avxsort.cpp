3b5af007be0deb9cddfb0f7beea51c6fc9865552
Shuhao Zhang <tonyzhang19900609@gmail.com>
2019-11-20 19:36:11 +0800

    add test module.

--- sorting/joins/avxsort.cpp
+++ sorting/joins/avxsort.cpp
@@ -18,7 +18,7 @@
 #endif
 
 
-extern int keycmp(const void *k1, const void *k2);
+extern int keycmp(const void * k1, const void * k2);
 
 /*******************************************************************************
  *                                                                             *
@@ -39,35 +39,36 @@ extern int keycmp(const void *k1, const void *k2);
  * @warning inputptr and outputptr might be swapped internally.
  */
 void
-avxsort_unaligned(int64_t **inputptr, int64_t **outputptr, uint64_t nitems) {
-    if (nitems <= 0)
+avxsort_unaligned(int64_t ** inputptr, int64_t ** outputptr, uint64_t nitems)
+{
+    if(nitems <= 0)
         return;
 
-    int64_t *input = *inputptr;
-    int64_t *output = *outputptr;
+    int64_t * input  = * inputptr;
+    int64_t * output = * outputptr;
 
     uint64_t i;
     uint64_t nchunks = (nitems / BLOCKSIZE);
     int rem = (nitems % BLOCKSIZE);
 
     /* each chunk keeps track of its temporary memory offset */
-    int64_t *ptrs[nchunks + 1][2];/* [chunk-in, chunk-out-tmp] */
-    uint32_t sizes[nchunks + 1];
+    int64_t * ptrs[nchunks+1][2];/* [chunk-in, chunk-out-tmp] */
+    uint32_t sizes[nchunks+1];
 
-    for (i = 0; i <= nchunks; i++) {
-        ptrs[i][0] = input + i * BLOCKSIZE;
+    for(i = 0; i <= nchunks; i++) {
+        ptrs[i][0] = input + i *  BLOCKSIZE;
         ptrs[i][1] = output + i * BLOCKSIZE;
-        sizes[i] = BLOCKSIZE;
+        sizes[i]   = BLOCKSIZE;
     }
 
     /** 1) Divide the input into chunks fitting into L2 cache. */
     /* one more chunk if not divisible */
-    for (i = 0; i < nchunks; i++) {
+    for(i = 0; i < nchunks; i++) {
         avxsort_block(&ptrs[i][0], &ptrs[i][1], BLOCKSIZE);
         swap(&ptrs[i][0], &ptrs[i][1]);
     }
 
-    if (rem) {
+    if(rem) {
         /* sort the last chunk which is less than BLOCKSIZE */
         avxsort_rem(&ptrs[i][0], &ptrs[i][1], rem);
         swap(&ptrs[i][0], &ptrs[i][1]);
@@ -82,30 +83,30 @@ avxsort_unaligned(int64_t **inputptr, int64_t **outputptr, uint64_t nitems) {
     nchunks += (rem > 0);
     /* printf("Merge chunks = %d\n", nchunks); */
     const uint64_t logN = ceil(log2(nitems));
-    for (i = LOG2_BLOCKSIZE; i < logN; i++) {
+    for(i = LOG2_BLOCKSIZE; i < logN; i++) {
 
         uint64_t k = 0;
-        for (uint64_t j = 0; j < (nchunks - 1); j += 2) {
-            int64_t *inpA = ptrs[j][0];
-            int64_t *inpB = ptrs[j + 1][0];
-            int64_t *out = ptrs[j][1];
-            uint32_t sizeA = sizes[j];
-            uint32_t sizeB = sizes[j + 1];
+        for(uint64_t j = 0; j < (nchunks-1); j += 2) {
+            int64_t * inpA  = ptrs[j][0];
+            int64_t * inpB  = ptrs[j+1][0];
+            int64_t * out   = ptrs[j][1];
+            uint32_t  sizeA = sizes[j];
+            uint32_t  sizeB = sizes[j+1];
 
             merge16_varlen(inpA, inpB, out, sizeA, sizeB);
 
             /* setup new pointers */
             ptrs[k][0] = out;
             ptrs[k][1] = inpA;
-            sizes[k] = sizeA + sizeB;
+            sizes[k]   = sizeA + sizeB;
             k++;
         }
 
-        if ((nchunks % 2)) {
+        if((nchunks % 2)) {
             /* just move the pointers */
-            ptrs[k][0] = ptrs[nchunks - 1][0];
-            ptrs[k][1] = ptrs[nchunks - 1][1];
-            sizes[k] = sizes[nchunks - 1];
+            ptrs[k][0] = ptrs[nchunks-1][0];
+            ptrs[k][1] = ptrs[nchunks-1][1];
+            sizes[k]   = sizes[nchunks-1];
             k++;
         }
 
@@ -113,8 +114,8 @@ avxsort_unaligned(int64_t **inputptr, int64_t **outputptr, uint64_t nitems) {
     }
 
     /* finally swap input/output pointers, where output holds the sorted list */
-    *outputptr = ptrs[0][0];
-    *inputptr = ptrs[0][1];
+    * outputptr = ptrs[0][0];
+    * inputptr  = ptrs[0][1];
 
 }
 
@@ -124,35 +125,36 @@ avxsort_unaligned(int64_t **inputptr, int64_t **outputptr, uint64_t nitems) {
  *                                                                             *
  *******************************************************************************/
 void
-avxsort_aligned(int64_t **inputptr, int64_t **outputptr, uint64_t nitems) {
-    if (nitems <= 0)
+avxsort_aligned(int64_t ** inputptr, int64_t ** outputptr, uint64_t nitems)
+{
+    if(nitems <= 0)
         return;
 
-    int64_t *input = *inputptr;
-    int64_t *output = *outputptr;
+    int64_t * input  = * inputptr;
+    int64_t * output = * outputptr;
 
     uint64_t i;
     uint64_t nchunks = (nitems / BLOCKSIZE);
     int rem = (nitems % BLOCKSIZE);
     /* printf("nchunks = %d, nitems = %d, rem = %d\n", nchunks, nitems, rem); */
     /* each chunk keeps track of its temporary memory offset */
-    int64_t *ptrs[nchunks + 1][2];/* [chunk-in, chunk-out-tmp] */
-    uint32_t sizes[nchunks + 1];
+    int64_t * ptrs[nchunks+1][2];/* [chunk-in, chunk-out-tmp] */
+    uint32_t sizes[nchunks+1];
 
-    for (i = 0; i <= nchunks; i++) {
-        ptrs[i][0] = input + i * BLOCKSIZE;
+    for(i = 0; i <= nchunks; i++) {
+        ptrs[i][0] = input + i *  BLOCKSIZE;
         ptrs[i][1] = output + i * BLOCKSIZE;
-        sizes[i] = BLOCKSIZE;
+        sizes[i]   = BLOCKSIZE;
     }
 
     /** 1) Divide the input into chunks fitting into L2 cache. */
     /* one more chunk if not divisible */
-    for (i = 0; i < nchunks; i++) {
+    for(i = 0; i < nchunks; i++) {
         avxsort_block_aligned(&ptrs[i][0], &ptrs[i][1], BLOCKSIZE);
         swap(&ptrs[i][0], &ptrs[i][1]);
     }
 
-    if (rem) {
+    if(rem) {
         /* sort the last chunk which is less than BLOCKSIZE */
         avxsort_rem_aligned(&ptrs[i][0], &ptrs[i][1], rem);
         swap(&ptrs[i][0], &ptrs[i][1]);
@@ -167,30 +169,30 @@ avxsort_aligned(int64_t **inputptr, int64_t **outputptr, uint64_t nitems) {
     nchunks += (rem > 0);
     /* printf("Merge chunks = %d\n", nchunks); */
     const uint64_t logN = ceil(log2(nitems));
-    for (i = LOG2_BLOCKSIZE; i < logN; i++) {
+    for(i = LOG2_BLOCKSIZE; i < logN; i++) {
 
         uint64_t k = 0;
-        for (uint64_t j = 0; j < (nchunks - 1); j += 2) {
-            int64_t *inpA = ptrs[j][0];
-            int64_t *inpB = ptrs[j + 1][0];
-            int64_t *out = ptrs[j][1];
-            uint32_t sizeA = sizes[j];
-            uint32_t sizeB = sizes[j + 1];
+        for(uint64_t j = 0; j < (nchunks-1); j += 2) {
+            int64_t * inpA  = ptrs[j][0];
+            int64_t * inpB  = ptrs[j+1][0];
+            int64_t * out   = ptrs[j][1];
+            uint32_t  sizeA = sizes[j];
+            uint32_t  sizeB = sizes[j+1];
 
             merge8_varlen_aligned(inpA, inpB, out, sizeA, sizeB);
 
             /* setup new pointers */
             ptrs[k][0] = out;
             ptrs[k][1] = inpA;
-            sizes[k] = sizeA + sizeB;
+            sizes[k]   = sizeA + sizeB;
             k++;
         }
 
-        if ((nchunks % 2)) {
+        if((nchunks % 2)) {
             /* just move the pointers */
-            ptrs[k][0] = ptrs[nchunks - 1][0];
-            ptrs[k][1] = ptrs[nchunks - 1][1];
-            sizes[k] = sizes[nchunks - 1];
+            ptrs[k][0] = ptrs[nchunks-1][0];
+            ptrs[k][1] = ptrs[nchunks-1][1];
+            sizes[k]   = sizes[nchunks-1];
             k++;
         }
 
@@ -198,45 +200,48 @@ avxsort_aligned(int64_t **inputptr, int64_t **outputptr, uint64_t nitems) {
     }
 
     /* finally swap input/output pointers, where output holds the sorted list */
-    *outputptr = ptrs[0][0];
-    *inputptr = ptrs[0][1];
+    * outputptr = ptrs[0][0];
+    * inputptr  = ptrs[0][1];
 
 }
 
 void
-avxsort_tuples(tuple_t **inputptr, tuple_t **outputptr, uint64_t nitems) {
-    int64_t *input = (int64_t *) (*inputptr);
-    int64_t *output = (int64_t *) (*outputptr);
+avxsort_tuples(tuple_t ** inputptr, tuple_t ** outputptr, uint64_t nitems)
+{
+    int64_t * input  = (int64_t*)(*inputptr);
+    int64_t * output = (int64_t*)(*outputptr);
 
     /* choose actual implementation depending on the input alignment */
-    if (((uintptr_t) input % CACHE_LINE_SIZE) == 0
-        && ((uintptr_t) output % CACHE_LINE_SIZE) == 0)
+    if(((uintptr_t)input % CACHE_LINE_SIZE) == 0
+       && ((uintptr_t)output % CACHE_LINE_SIZE) == 0)
         avxsort_aligned(&input, &output, nitems);
     else
         avxsort_unaligned(&input, &output, nitems);
 
-    *inputptr = (tuple_t *) (input);
-    *outputptr = (tuple_t *) (output);
+    *inputptr = (tuple_t *)(input);
+    *outputptr = (tuple_t *)(output);
 }
 
 void
-avxsort_int64(int64_t **inputptr, int64_t **outputptr, uint64_t nitems) {
+avxsort_int64(int64_t ** inputptr, int64_t ** outputptr, uint64_t nitems)
+{
     /* \todo: implement */
-    int64_t *input = (int64_t *) (*inputptr);
-    int64_t *output = (int64_t *) (*outputptr);
+    int64_t * input  = (int64_t*)(*inputptr);
+    int64_t * output = (int64_t*)(*outputptr);
 
     /* choose actual implementation depending on the input alignment */
-    if (((uintptr_t) input % CACHE_LINE_SIZE) == 0
-        && ((uintptr_t) output % CACHE_LINE_SIZE) == 0)
+    if(((uintptr_t)input % CACHE_LINE_SIZE) == 0
+       && ((uintptr_t)output % CACHE_LINE_SIZE) == 0)
         avxsort_aligned(&input, &output, nitems);
     else
         avxsort_unaligned(&input, &output, nitems);
 
-    *inputptr = (int64_t *) (input);
-    *outputptr = (int64_t *) (output);
+    *inputptr = (int64_t *)(input);
+    *outputptr = (int64_t *)(output);
 }
 
 void
-avxsort_int32(int32_t **inputptr, int32_t **outputptr, uint64_t nitems) {
+avxsort_int32(int32_t ** inputptr, int32_t ** outputptr, uint64_t nitems)
+{
     /* \todo: implement */
 }
