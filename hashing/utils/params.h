35fb18fbc9951f89f47aa9fc627506d4c41edc28
Shuhao Zhang <tonyzhang19900609@gmail.com>
2020-05-07 22:24:12 +0200

    retest large NUMA

--- hashing/utils/params.h
+++ hashing/utils/params.h
@@ -33,9 +33,9 @@
 #define L2_CACHE_SIZE (256*1024)
 #endif
 
-/** L3 Cache size of the system in bytes */
+/** L3 Cache size of the system in bytes 20480 or */
 #ifndef L3_CACHE_SIZE
-#define L3_CACHE_SIZE (20480*1024)
+#define L3_CACHE_SIZE (25600*1024)
 #endif
 
 /**
