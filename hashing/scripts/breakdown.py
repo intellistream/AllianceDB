7cee749b9292460640159a56ab6898dde1c3f307
tony <tonyzhang19900609@gmail.com>
2020-09-22 11:02:49 +0200

    code format.

--- hashing/scripts/breakdown.py
+++ hashing/scripts/breakdown.py
@@ -84,7 +84,7 @@ def DrawFigure(x_values, y_values, y_max, legend_labels, x_label, y_label, filen
     # plt.ylim(0, y_max)
     # plt.yscale('log')
     plt.ticklabel_format(axis="y", style="sci", scilimits=(0, 0))
-    figure.yaxis.set_major_locator(MaxNLocator(integer=True))
+    figure.yaxis.set_major_locator(MaxNLocator(nbins=4, integer=True))
     # you may need to tune the xticks position to get the best figure.
     plt.xticks(index + 0.5 * width, x_values)
     # plt.autofmt_xdate()
