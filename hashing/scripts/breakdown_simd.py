c6b44f934c532ca003173a15b5b0f154641bff50
tony <tonyzhang19900609@gmail.com>
2020-11-22 18:06:42 +0100

    update figure axis

--- hashing/scripts/breakdown_simd.py
+++ hashing/scripts/breakdown_simd.py
@@ -111,7 +111,7 @@ def DrawFigure(x_values, y_values, legend_labels, x_label, y_label, filename, al
     figure.get_yaxis().set_major_formatter(yfmt)
     # plt.ticklabel_format(axis="y", style="sci", scilimits=(0, 0))
 
-    # plt.ticklabel_format(axis="y", style="sci", scilimits=(0,0), useMathText=True)
+    plt.ticklabel_format(axis="y", style="sci", scilimits=(0,0), useMathText=True)
     plt.grid(axis='y', color='gray')
     figure.yaxis.set_major_locator(LinearLocator(3))
 
