b06156af5dc49d92a01672672b53c5fb6c53d34d
tony <tonyzhang19900609@gmail.com>
2020-11-25 15:47:53 +0100

    update figure axis

--- hashing/scripts/breakdown_p_np_study.py
+++ hashing/scripts/breakdown_p_np_study.py
@@ -115,7 +115,7 @@ def DrawFigure(x_values, y_values, legend_labels, x_label, y_label, y_min, y_max
             prop=LEGEND_FP,
             ncol=1,
             # bbox_to_anchor=(0.5, 1.3),
-            bbox_to_anchor=(1.25, 0.5),
+            bbox_to_anchor=(1.3, 0.5),
             handletextpad=0.2,
             borderaxespad=0.0,
             handlelength=1.8,
