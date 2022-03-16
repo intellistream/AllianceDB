3ae5cc5c3a72e001b93d032a441caaea7a7c0461
tony <292224750@.com>
2022-03-16 10:57:44 +0800

    issue #30: 1. Configurable slide for CellJoin 2. Fixed some bugs 3. Add celljoin to test bench

--- benchmark/src/Benchmark.cpp
+++ benchmark/src/Benchmark.cpp
@@ -46,9 +46,17 @@ int main() {
   AbstractJoinMethod<AbstractLazyWS> lwj;
  // INTELLI::UtilityFunctions::timerStart(joinResult);
   lwj.test(joinResult,relationCouple);
-
   //Print result number
  // INTELLI::UtilityFunctions::timerEnd(joinResult);
+  joinResult.statPrinter();
+  dataSet.load3VText(relationCouple.relationR, fileRName);
+  dataSet.load3VText(relationCouple.relationS, fileSName);
+  joinResult.streamSize = relationCouple.relationR.size();
+  AbstractJoinMethod<AbstractEagerWS> ewj;
+  // INTELLI::UtilityFunctions::timerStart(joinResult);
+  ewj.test(joinResult,relationCouple);
+  //Print result number
+  // INTELLI::UtilityFunctions::timerEnd(joinResult);
   joinResult.statPrinter();
   /*MicroDataSet mr(999);
   DatasetTool dataSet;
