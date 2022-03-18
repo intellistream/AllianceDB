0101282f6e1cef0c368b7aa623fa76d4fd10f175
tony <292224750@.com>
2022-03-18 11:43:18 +0800

    issue #35 1. Add the VerifyWS class in window slider, which is a single thread reference for testing other sliders 2. Add the VerfyBench component in common 3. Edit benchmark and simpletest accordingly. 4. Reformat code. 5. Fix bugs in SimpleTest.cpp

--- test/SystemTest/SimpleTest.cpp
+++ test/SystemTest/SimpleTest.cpp
@@ -30,7 +30,7 @@ TEST(SystemTest, SimpleTest
   dataSet.load3VText(relationCouple.relationR, fileRName);
   dataSet.load3VText(relationCouple.relationS, fileSName);
   joinResult.streamSize = relationCouple.relationR.size();
-  VerifyBench<AbstractLazyWS> vb_ewj;
+  VerifyBench<AbstractEagerWS> vb_ewj;
   ASSERT_TRUE(vb_ewj.test(joinResult, relationCouple, 1));
   dataSet.load3VText(relationCouple.relationR, fileRName);
   dataSet.load3VText(relationCouple.relationS, fileSName);
@@ -38,6 +38,9 @@ TEST(SystemTest, SimpleTest
   dataSet.load3VText(relationCouple.relationR, fileRName);
   dataSet.load3VText(relationCouple.relationS, fileSName);
   ASSERT_TRUE(vb_ewj.test(joinResult, relationCouple, 4));
+  dataSet.load3VText(relationCouple.relationR, fileRName);
+  dataSet.load3VText(relationCouple.relationS, fileSName);
+  ASSERT_TRUE(vb_ewj.test(joinResult, relationCouple, 4,500,50));
   /* AbstractJoinMethod<AbstractEagerWS> ewj;
    // INTELLI::UtilityFunctions::timerStart(joinResult);
    ewj.test(joinResult, relationCouple);
