07b75899636ff7a299a2e1ccdc1c58fa070e8b3f
BlackJackTone <tangxilin2001@qq.com>
2022-04-14 16:54:02 +0800

    script modified

--- hashing/scripts/draw_NUMA.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#### APP Scale.
-#python3 throughput_scale_lazy.py
-#python3 throughput_scale_eager.py
-
-python3 throughput_scale_stock.py
-python3 throughput_scale_rovio.py
-python3 throughput_scale_ysb.py
-python3 throughput_scale_debs.py
